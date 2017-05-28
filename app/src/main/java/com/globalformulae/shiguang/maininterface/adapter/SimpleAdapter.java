5097f1df3d74f5bffc1ec39328853acd200be593
Zxg444363021 <444363021@qq.com>
2017-05-28 22:28:49 +0800

    新增互动记录

---
+++ app/src/main/java/com/globalformulae/shiguang/maininterface/adapter/SimpleAdapter.java
@@ -0,0 +1,126 @@
+package com.globalformulae.shiguang.maininterface.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.globalformulae.shiguang.R;
+import com.globalformulae.shiguang.model.AlternateRecord;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * timer页面的5条记录
+ * Created by ZXG on 2017/5/28.
+ */
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleRecordViewHolder> {
+    private Context mContext;
+    private List<AlternateRecord> mDatas;
+    private LayoutInflater mInflater;
+
+    public SimpleAdapter(Context context, List<AlternateRecord> datas) {
+        mContext=context;
+        mDatas=datas;
+        mInflater=LayoutInflater.from(context);
+    }
+
+    @Override
+    public SimpleRecordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=mInflater.inflate(R.layout.adapter_simple_record,parent,false);
+        SimpleRecordViewHolder viewHolder=new SimpleRecordViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleRecordViewHolder holder, int position) {
+        holder.simpleRecordUserTV.setText(mDatas.get(position).getUser1name());
+        if(mDatas.get(position).getType()==1||mDatas.get(position).getType()==2){
+           holder.simpleRecordActionTV.setText("收取"+mDatas.get(position).getPower()+"g");
+        }else{
+            holder.simpleRecordActionTV.setText("来浇过水，+20g能量");
+        }
+        holder.simpleRecordTimeTV.setText(date2string(mDatas.get(position).getDate()));
+
+    }
+
+    /**
+     * 最多只显示5条记录
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        if (mDatas.size()>=5){
+            return 5;
+        }else{
+            return mDatas.size();
+        }
+
+    }
+
+    /**
+     * 将日期转换为“昨天”...
+     * @param date
+     * @return
+     */
+    private  String date2string(Date date){
+        Calendar today=Calendar.getInstance();
+        System.out.println(today.getTime().toString());
+        Calendar theDay=Calendar.getInstance();
+        theDay.setTime(date);
+        System.out.println(theDay.getTime().toString());
+        Calendar oneDay=Calendar.getInstance();;
+        oneDay.add(Calendar.DAY_OF_MONTH, -2);
+        System.out.println(oneDay.getTime().toString());
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "昨天";
+        }
+        oneDay.add(Calendar.DAY_OF_MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -1);
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "1天前";
+        }
+        oneDay.add(Calendar.DAY_OF_MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -1);
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "2天前";
+        }
+        oneDay.add(Calendar.DAY_OF_MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -1);
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "3天前";
+        }
+        oneDay.add(Calendar.DAY_OF_MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -1);
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "4天前";
+        }
+        oneDay.add(Calendar.DAY_OF_MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -1);
+        if(theDay.before(today)&&theDay.after(oneDay)){
+            return "5天前";
+        }else{
+            java.util.Date  date2=theDay.getTime();
+            return String.valueOf(date2.getMonth()+"-"+date2.getDate());
+        }
+
+    }
+}
+class SimpleRecordViewHolder extends RecyclerView.ViewHolder{
+    TextView simpleRecordUserTV;
+    TextView simpleRecordActionTV;
+    TextView simpleRecordTimeTV;
+
+
+    public SimpleRecordViewHolder(View itemView) {
+        super(itemView);
+        simpleRecordUserTV= (TextView) itemView.findViewById(R.id.simple_record_user_tv);
+        simpleRecordActionTV= (TextView) itemView.findViewById(R.id.simple_record_action_tv);
+        simpleRecordTimeTV= (TextView) itemView.findViewById(R.id.simple_record_time_tv);
+    }
+}
---
+++ app/src/main/java/com/globalformulae/shiguang/model/AlternateRecord.java
@@ -0,0 +1,82 @@
+package com.globalformulae.shiguang.model;
+
+import java.sql.Date;
+
+/**
+ * Created by ZXG on 2017/5/28.
+ */
+
+public class AlternateRecord {
+    private Long id;
+    private Long user1id;
+    private Long user2id;
+    private int type;
+    private int power;
+    private String user1name;
+    private String user2name;
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUser1name() {
+        return user1name;
+    }
+
+    public void setUser1name(String user1name) {
+        this.user1name = user1name;
+    }
+
+    public String getUser2name() {
+        return user2name;
+    }
+
+    public void setUser2name(String user2name) {
+        this.user2name = user2name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser1id() {
+        return user1id;
+    }
+
+    public void setUser1id(Long user1id) {
+        this.user1id = user1id;
+    }
+
+    public Long getUser2id() {
+        return user2id;
+    }
+
+    public void setUser2id(Long user2id) {
+        this.user2id = user2id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+}
